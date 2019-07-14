78440554c5e8c79629677b76f4ac38d34ffe0b91
Antti Manninen <antti.e.manninen@tuni.fi>
2019-07-14 16:21:59 +0300

    1.5 Kurssitiedot step4 & step5 DONE

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 const Header = (props) => {
     return (
-        <h1>{props.course}</h1>
+        <h1>{props.course.name}</h1>
     )
 }
 
@@ -28,31 +28,35 @@ const Content = (props) => {
 const Total = (props) => {
     return (
         <p>
-            Number of exercises { props.total }
+            Number of exercises { props.total[0].exercises + props.total[1].exercises + props.total[2].exercises }
         </p>
     )
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
     }
 
     return (
         <div>
             <Header course={course} />
-            <Content parts={[part1, part2, part3]} />
-            <Total total={part1.exercises + part2.exercises + part3.exercises} />
+            <Content parts={course.parts} />
+            <Total total={course.parts} />
         </div>
     )
 }
