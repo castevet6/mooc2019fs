e67d3cceb3b1de44c66a307b62a33898485f5246
Antti Manninen <antti.e.manninen@tuni.fi>
2019-07-17 22:51:27 +0300

    1.6 unicafe step1 DONE

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+// komponentti napille
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
+
+// Display-komponentti joka esittää nimen ja arvon (esim. good 0)
+const Display = ({ name, value }) => (
+    <div>
+        {name} {value}
+    </div>
+)
+
+const App = () => {
+    // tilat eri palautteille
+    const [ good, setGood ] = useState(0)
+    const [ neutral, setNeutral ] = useState(0)
+    const [ bad, setBad ] = useState(0)
+
+    // tapahtumakäsittelijät nappien painalluksille
+    const handleGoodClick = () => setGood(good + 1)
+    const handleNeutralClick = () => setNeutral(neutral + 1)
+    const handleBadClick = () => setBad(bad + 1)
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <Button handleClick={handleGoodClick} text='good' />
+            <Button handleClick={handleNeutralClick} text='neutral' />
+            <Button handleClick={handleBadClick} text='bad' />
+            <h1>Statistics</h1>
+            <Display name='good' value={good} />
+            <Display name='neutral' value={neutral} />
+            <Display name='bad' value={bad} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
