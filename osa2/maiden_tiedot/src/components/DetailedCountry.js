11677b49505d592821241d05e835a26537df7bff
Antti Manninen <antti.e.manninen@tuni.fi>
2019-07-23 22:00:02 +0300

    2.13 maiden tiedot, step 3 KESKEN

--- osa2/maiden_tiedot/src/components/DetailedCountry.js
+++ osa2/maiden_tiedot/src/components/DetailedCountry.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const DetailedCountry = ({country}) => {
+
+    const [ weather, setWeather ] = useState({})
+    const baseUrl = 'http://api.apixu.com/v1/current.json?'
+    const apiKey = '631e1fa5e6f4495fbcd170613192307'
+    const url = `${baseUrl}key=${apiKey}&q=${country.capital.toLowerCase()}`
+
+    useEffect(() => {
+        axios.get(url)
+            .then(response => setWeather(response.data))
+    }, [])
 
     const showLanguages = () => country.languages.map(lang => <li key={lang.name}>{lang.name}</li>)
 
@@ -14,6 +25,10 @@ const DetailedCountry = ({country}) => {
                 {showLanguages()}
             </ul>
             <img src={country.flag} width="120" alt={"flag of " + country.name} />
+            <h2>Weather in {country.capital}</h2>
+            <b>temperature: </b> {weather.current.temp_c || 'loading'} Celsius
+            <img src={weather.current.condition.icon || 'loading'} alt="weather icon" />
+        
         </div>
     )
 }
