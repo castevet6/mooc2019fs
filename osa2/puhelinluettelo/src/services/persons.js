00ab2612d788facab9013b87dab58184f7e70df0
Antti Manninen <antti.e.manninen@tuni.fi>
2019-08-19 13:14:59 +0300

    3.11 fixed base URL to absolute Heroku URL

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = '/api/persons'
+const baseUrl = 'https://fsopen-puhelinluettelo.herokuapp.com/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
